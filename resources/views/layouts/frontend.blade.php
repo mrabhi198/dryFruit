c673d12cb66d1420dce9573c6cfe4e8361069966
mrabhi198 <abhinavalok318@gmail.com>
2025-05-02 00:40:04 +0530

    some parts are fixed

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -2,23 +2,21 @@
 <html lang="en">
 <head>
     <title>{{ $title ?? 'Sawaikar Dry Fruits' }}</title>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="keywords" content="{{ $title ?? 'Suresh Electronics' }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Meta -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="keywords" content="{{ $title ?? 'Sawaikar Dry Fruits' }}">
     <meta property="og:locale" content="en_US"/>
     <meta property="og:type" content="website"/>
     <meta property="og:image" content="{{ asset('assets/img/cart.png') }}">
-    
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+    <!-- Icons & Styles -->
     <link rel="shortcut icon" href="{{ asset('assets/img/cart.png') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet">
-    <link rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-          integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
-          crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
 <body>
 <!-- Navbar -->
@@ -26,15 +24,13 @@
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}"><b>Sawaikar</b></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon text-dark pt-2"><i class="fas fa-bars"></i></span>
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon text-dark pt-2"><i class="bi bi-list"></i></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" href="{{ url('/') }}">Home</a>
-                </li>
+                <li class="nav-item"><a class="nav-link active" href="{{ url('/') }}">Home</a></li>
                 @foreach($categories as $category)
                     <li class="nav-item">
                         <a class="nav-link active" href="{{ url('category/' . $category->id) }}">
@@ -50,9 +46,8 @@
                     @endif
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" href="#" data-bs-toggle="modal"
-                       data-bs-target="#searchModal" aria-disabled="true">
-                        <i class="fas fa-search"></i>
+                    <a class="nav-link active" href="#" data-bs-toggle="modal" data-bs-target="#searchModal">
+                        <i class="bi bi-search"></i>
                     </a>
                 </li>
             </ul>
@@ -60,15 +55,9 @@
     </div>
 </nav>
 
-@php 
-    $profile = App\Models\User::find(1);
-@endphp
-
 <!-- Main content -->
 <div class="clearfix mt-5 pt-4"></div>
-<div class="main">
-    @yield('content')
-</div>
+<div class="main">@yield('content')</div>
 
 <!-- Footer -->
 <div class="footer">
@@ -76,66 +65,51 @@
         <div class="row">
             <div class="col-sm-8">
                 <h4>Need Assistance?</h4>
-                <i class="bi bi-clock"></i> 09:00 AM - 09:00 PM (Mon - Sun) <br>
-                <i class="bi bi-envelope"></i> sawaikarcashewstore1980@gmail.com <br>
-                <i class="bi bi-geo-alt"></i> Chogm Rd, Porvorim, Aradi Socorro, Goa 403521 <br>
-
+                <i class="bi bi-clock"></i> 09:00 AM - 09:00 PM (Mon - Sun)<br>
+                <i class="bi bi-envelope"></i> sawaikarcashewstore1980@gmail.com<br>
+                <i class="bi bi-geo-alt"></i> Chogm Rd, Porvorim, Aradi Socorro, Goa 403521<br>
             </div>
             <div class="col-sm-4">
                 <h4>Stay Connected</h4>
-                <!-- <a href="https://www.instagram.com/sawaikarsgoancashews" class="text-connected"><i class="fab fa-instagram"></i></a>
-                <a href="#" class="text-connected"><i class="fab fa-twitter"></i></a>
-                <a href="#" class="text-connected"><i class="fab fa-youtube"></i></a>
-                <a href="#" class="text-connected"><i class="fab fa-linkedin"></i></a> -->
-                <i class="bi bi-envelope"></i> sawaikarcashewstore1980@gmail.com <br>
-                <i class="bi bi-instagram"></i> @sawaikarsgoancashews <br>
-                <i class="bi bi-telephone"></i> +91 950 322 3046 <br>
-
+                <i class="bi bi-envelope"></i> sawaikarcashewstore1980@gmail.com<br>
+                <i class="bi bi-instagram"></i> @sawaikarsgoancashews<br>
+                <i class="bi bi-telephone"></i> +91 950 322 3046<br>
             </div>
         </div>
     </div>
 </div>
 
 <!-- Copyright -->
-<div class="copyright">
-    <div class="container text-center">
-        Copyright &copy; {{ date('Y') }} Sawaikar. All rights reserved.
-        <br>
+<div class="copyright text-center py-2">
+    <div class="container">
+        &copy; {{ date('Y') }} Sawaikar. All rights reserved.
     </div>
 </div>
 
 <!-- Search Modal -->
-<div class="modal fade" id="searchModal" data-bs-backdrop="static" data-bs-keyboard="false"
-     tabindex="-1" aria-labelledby="searchModalLabel" aria-hidden="true">
+<div class="modal fade" id="searchModal" tabindex="-1" aria-labelledby="searchModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">
-                    <i class="fas fa-search me-2"></i> Search Products
-                </h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <form method="get" action="{{ url('search') }}">
+        <form method="get" action="{{ url('search') }}">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title"><i class="bi bi-search me-2"></i> Search Products</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
                 <div class="modal-body">
-                    <div class="form-group">
-                        <input type="text" name="keyword" autocomplete="off" class="form-control" placeholder="Search here...">
-                    </div>
+                    <input type="text" name="keyword" autocomplete="off" class="form-control" placeholder="Search here...">
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                     <button type="submit" class="btn btn-primary">Search</button>
                 </div>
-            </form>
-        </div>
+            </div>
+        </form>
     </div>
 </div>
 
 <!-- Scripts -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"
-        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 @yield('javascript')
 </body>
 </html>
